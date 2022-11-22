77bf500f52e7cea0de4ec7f317189f904e6559be
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2022-11-23 01:24:53 +0200

    fix error in testbench

--- TestBench/Building_Blocks/IR_tb.v
+++ TestBench/Building_Blocks/IR_tb.v
@@ -2,9 +2,9 @@
 module IR_tb;
 	parameter PERIOD = 2;
 	reg write_enable, clk, rst;
-	reg [31:0] in;
-	wire [31:0] out;
-	PC pc(write_enable, clk, rst, in, out);
+	reg [15:0] in;
+	wire [15:0] out;
+	IR ir(write_enable, clk, rst, in, out);
 
 	initial begin
 		clk = 0;
@@ -12,7 +12,6 @@ module IR_tb;
 		write_enable = 0;
 		in = 0;	
 	end
-	
 	always #PERIOD clk=~clk;
 	
 	initial begin
@@ -23,9 +22,9 @@ module IR_tb;
 		in = 1234;	
 		#10;
 		if (out == 0)begin
-			$display("PASS test reset with write enable out = %d",out);	
+			$display("PASS test reset without write enable out = %d",out);	
 		end else begin
-			$display("Failed test reset with write enable out = %d",out);	
+			$display("Failed test reset without write enable out = %d",out);	
 		end
 	
 		// test reset with write enable
@@ -53,9 +52,9 @@ module IR_tb;
 		// test write
 		rst = 1;
 		write_enable = 1;
-		in = 9999_9999;	
+		in = 9999;	
 		#10;
-		if (out == 9999_9999)begin
+		if (out == 9999)begin
 			$display("PASS test read out = %d",out);	
 		end else begin
 			$display("Failed test read out = %d",out);	
@@ -64,9 +63,9 @@ module IR_tb;
 		// test write
 		rst = 1;
 		write_enable = 1;
-		in = 1234_9876;
+		in = 1234;
 		#10;
-		if (out == 1234_9876)begin
+		if (out == 1234)begin
 			$display("PASS test write out = %d",out);	
 		end else begin
 			$display("Failed test write out = %d",out);	
