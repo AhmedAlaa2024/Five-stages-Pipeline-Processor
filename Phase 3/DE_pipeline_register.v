c5097c21a45b2eb6bfe657736ad423ef0ae7daf1
Ahmed Alaa <62505107+AhmedAlaa2024@users.noreply.github.com>
2022-12-12 23:18:12 +0200

    Update DE_pipeline_register

--- Phase 3/DE_pipeline_register.v
+++ Phase 3/DE_pipeline_register.v
@@ -1,7 +1,10 @@
 module DE_pipeline_register #(parameter NUMBER_CONTROL_SIGNALS = 14) (control_sinals_IN, control_sinals_OUT, 
                              reg_dst_num_IN, reg_dst_num_OUT,
+                             reg_dst_value_IN, reg_dst_value_OUT,
                              reg_src_1_num_IN, reg_src_1_num_OUT,
+                             reg_src_1_value_IN, reg_src_1_value_OUT,
                              reg_src_2_num_IN, reg_src_2_num_OUT,
+                             reg_src_2_value_IN, reg_src_2_value_OUT,
                              address_IN, address_OUT,
                              clk, reset);
 
@@ -13,13 +16,25 @@ input [2:0] reg_dst_num_IN;
 output [2:0] reg_dst_num_OUT;
 reg [2:0] reg_dst_num_REG;
 
+input [2:0] reg_dst_value_IN;
+output [2:0] reg_dst_value_OUT;
+reg [2:0] reg_dst_value_REG;
+
 input [3:0] reg_src_1_num_IN;
 output [3:0] reg_src_1_num_OUT;
 reg [3:0] reg_src_1_num_REG;
 
+input [3:0] reg_src_1_value_IN;
+output [3:0] reg_src_1_value_OUT;
+reg [3:0] reg_src_1_value_REG;
+
 input [3:0] reg_src_2_num_IN;
 output [3:0] reg_src_2_num_OUT;
 reg [3:0] reg_src_2_num_REG;
+
+input [3:0] reg_src_2_value_IN;
+output [3:0] reg_src_2_value_OUT;
+reg [3:0] reg_src_2_value_REG;
 
 input [15:0] address_IN;
 output [15:0] address_OUT;
@@ -31,8 +46,11 @@ input reset;
 // Asynchronous read
 assign control_sinals_OUT = control_sinals_REG;
 assign reg_dst_num_OUT = reg_dst_num_REG;
+assign reg_dst_value_OUT = reg_dst_value_REG;
 assign reg_src_1_num_OUT = reg_src_1_num_REG;
+assign reg_src_1_value_OUT = reg_src_1_value_REG;
 assign reg_src_2_num_OUT = reg_src_2_num_REG;
+assign reg_src_2_value_OUT = reg_src_2_value_REG;
 assign address_OUT = address_REG;
 
 always @(posedge clk)
@@ -42,8 +60,11 @@ always @(posedge clk)
             begin
                 control_sinals_REG = 0;
                 reg_dst_num_REG = 0;
+                reg_dst_value_REG = 0;
                 reg_src_1_num_REG = 0;
+                reg_src_1_value_REG = 0;
                 reg_src_2_num_REG = 0;
+                reg_src_2_value_REG = 0;
                 address_REG = 0;
             end
         // Synchronous write @ +ve edge
@@ -51,8 +72,11 @@ always @(posedge clk)
             begin
                 control_sinals_REG = control_sinals_IN;
                 reg_dst_num_REG = reg_dst_num_IN;
+                reg_dst_value_REG = reg_dst_value_IN;
                 reg_src_1_num_REG = reg_src_1_num_IN;
+                reg_src_1_value_REG = reg_src_1_value_IN;
                 reg_src_2_num_REG = reg_src_2_num_IN;
+                reg_src_2_value_REG = reg_src_2_value_IN;
                 address_REG = address_IN;
             end
     end
