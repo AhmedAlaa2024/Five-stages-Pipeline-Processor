5d17f7f6f3869da6c9960e79f8dfdca9388c93fa
Ahmed Alaa <62505107+AhmedAlaa2024@users.noreply.github.com>
2022-12-27 19:29:21 +0200

    Adding SP in DE_pipeline_register

--- Phase 3/PipelineProcessor/DE_pipeline_register.v
+++ Phase 3/PipelineProcessor/DE_pipeline_register.v
@@ -6,6 +6,7 @@ module DE_pipeline_register #(parameter NUMBER_CONTROL_SIGNALS = 16) (control_si
                              reg_src_2_num_IN, reg_src_2_num_OUT,
                              reg_src_2_value_IN, reg_src_2_value_OUT,
                              address_IN, address_OUT,
+                             SP_value_IN, SP_value_OUT,
                              clk, reset,en);
 
 input [NUMBER_CONTROL_SIGNALS-1:0] control_sinals_IN;
@@ -40,6 +41,10 @@ input [15:0] address_IN;
 output [15:0] address_OUT;
 reg [15:0] address_REG;
 
+input [31:0] SP_value_IN;
+output [31:0] SP_value_OUT;
+reg [31:0] SP_value_REG;
+
 input clk;
 input reset;
 input en;
@@ -53,6 +58,7 @@ assign reg_src_1_value_OUT =(en)?  reg_src_1_value_REG:0;
 assign reg_src_2_num_OUT = (en)? reg_src_2_num_REG:0;
 assign reg_src_2_value_OUT = (en)? reg_src_2_value_REG:0;
 assign address_OUT = (en)? address_REG:0;
+assign SP_value_OUT = (en)? SP_value_REG:0;
 
 always @(posedge clk)
     begin
@@ -67,6 +73,7 @@ always @(posedge clk)
                 reg_src_2_num_REG = 0;
                 reg_src_2_value_REG = 0;
                 address_REG = 0;
+                SP_value_REG = 0;
             end
         // Synchronous write @ +ve edge
         else
@@ -80,6 +87,7 @@ always @(posedge clk)
                     reg_src_2_num_REG = reg_src_2_num_IN;
                     reg_src_2_value_REG = reg_src_2_value_IN;
                     address_REG = address_IN;
+                    SP_value_REG = SP_value_IN;
                 end
                 // else begin
                 // control_sinals_REG = 0;
